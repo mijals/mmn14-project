4262350f21468f4e684347865c8f8555c3538a86
mijals <41447582+mijals@users.noreply.github.com>
2018-07-22 15:19:24 +0300

    Update addressing.c

--- addressing.c
+++ addressing.c
@@ -2,15 +2,15 @@
 
 /*currently assuming correct input*/
 
-char[] addressing(char[] operand)
+int addressing(char[] operand)
 {
   int i;
   if (operand[0] == '#')
-    return "00";
+    return 0;
   if (operand[0] == 'r' && opernd[1] >= 48 && operand[1] <= 55 && operand[2] == null)
-    return "11";
+    return 3;
   for(i=0; i<strlen(operand); i++)
     if (operand[i] == '(')
-      return "10";
-  return "01"; 
+      return 2;
+  return 1; 
 }
